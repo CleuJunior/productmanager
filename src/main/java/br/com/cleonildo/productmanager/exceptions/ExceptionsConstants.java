628b72b0b191a95432cfb65763ddfe68f5ebc13f
CleuJunior <cleu.junior@gmail.com>
2023-12-07 19:40:39 -0300

    feat: Product exception

--- src/main/java/br/com/cleonildo/productmanager/exceptions/ExceptionsConstants.java
+++ src/main/java/br/com/cleonildo/productmanager/exceptions/ExceptionsConstants.java
@@ -6,5 +6,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionsConstants {
     public static final String CATEGORY_ID_NOT_FOUND = "Category not found!";
+    public static final String PRODUCT_ID_NOT_FOUND = "Product not found!";
 
 }
