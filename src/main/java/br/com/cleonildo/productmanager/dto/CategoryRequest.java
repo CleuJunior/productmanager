a5a3be4b1cf69598c7bae5adb7fc46ac9a81d419
CleuJunior <cleu.junior@gmail.com>
2023-12-08 09:30:02 -0300

    feat: Validation

--- src/main/java/br/com/cleonildo/productmanager/dto/CategoryRequest.java
+++ src/main/java/br/com/cleonildo/productmanager/dto/CategoryRequest.java
@@ -1,6 +1,16 @@
 package br.com.cleonildo.productmanager.dto;
 
 import br.com.cleonildo.productmanager.entities.CategoryType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
-public record CategoryRequest(String name, boolean active, CategoryType type) {
-}
+public record CategoryRequest(
+        @NotBlank
+        @NotEmpty
+        String name,
+        @NotNull
+        boolean active,
+        @NotNull
+        CategoryType type
+) { }
--- src/main/java/br/com/cleonildo/productmanager/dto/ProductRequest.java
+++ src/main/java/br/com/cleonildo/productmanager/dto/ProductRequest.java
@@ -1,5 +1,6 @@
 package br.com.cleonildo.productmanager.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
@@ -7,25 +8,32 @@ import jakarta.validation.constraints.PositiveOrZero;
 import java.util.Date;
 
 public record ProductRequest(
-        @NotNull
+        @NotBlank
         @NotEmpty
         String name,
-        boolean active,
         @NotNull
+        boolean active,
+        @NotBlank
         @NotEmpty
         String sku,
         @NotNull
         @NotEmpty
         String image,
+        @NotNull
         Date registrationDate,
+        @NotNull
         @PositiveOrZero
         int stockQuantity,
+        @NotNull
         @PositiveOrZero
         double costValue,
+        @NotNull
         @PositiveOrZero
         double icms,
+        @NotNull
         @PositiveOrZero
         double saleValue,
+        @NotNull
         @PositiveOrZero
         Long categoryId
 ) { }
